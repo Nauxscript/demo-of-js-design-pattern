d0aa1e9e2cb2ffde4488e2a0c8eb8ebe884500a7
nauxscript <nauxscript@gmail.com>
2022-11-08 14:51:57 +0800

    refactor: updated demo

--- polymorphism/index.js
+++ polymorphism/index.js
@@ -1,6 +1,7 @@
 // 多态：同一操作作用于不同的对象上面，可以产生不同的解释和不同的执行结果。
 
 // DEMO: 对于鸭子和狗子来说，其都可以叫，只不过当我们让它们叫时，一个“嘎嘎”叫，一个“汪汪”叫。
+// 当我们调用 makesound 时，我们只在意让它们叫，至于它们内部怎么叫，我们不关心。
 
 class Dog {
   constructor() {}
@@ -18,12 +19,12 @@ class Duck {
   }
 }
 
-const makesound = (obj) => {
+const makeSound = (obj) => {
   obj.bark()
 }
 
 const dog = new Dog()
 const duck = new Duck()
 
-makesound(dog)
-makesound(duck)
+makeSound(dog)
+makeSound(duck)

914ee9ac56601a91a8ec94c3b7c7f710399e34b9
nauxscript <nauxscript@gmail.com>
2023-03-30 15:15:35 +0800

    refactor: test example

--- examples/strategy-pattern/LoginMethods/test.spec.ts
+++ examples/strategy-pattern/LoginMethods/test.spec.ts
@@ -1,7 +1,8 @@
 import { describe, expect, test } from 'vitest'
-import { methodStrategies } from '.'
+import type { Form, User } from '.'
+import { Methods, methodStrategies } from '.'
 
-const getUserInfo = () => ({
+const getUserInfo = (): User => ({
   userId: 'root',
   userPhone: '13838383838',
 })
@@ -10,18 +11,16 @@ describe('Strategy Pattern: Third-Party Login Methods', () => {
   describe('when the type of manufacturer is MF1', () => {
     test('when the type of manufacturer is MF1 and method type is qrCode', () => {
       const userInfo = getUserInfo()
-      const method = 'qrCode'
-      const requestData = methodStrategies[method](userInfo)
-      expect(requestData.model).toEqual(method)
+      const requestData = methodStrategies[Methods.qrCode](userInfo)
+      expect(requestData.model).toEqual(Methods.qrCode)
       expect(requestData.damnParamA).toBeDefined()
       expect(requestData.userCode).toEqual(userInfo.userId)
     })
 
     test('when the type of manufacturer is MF1 and method type is SMS', () => {
       const userInfo = getUserInfo()
-      const method = 'SMS'
-      const requestData = methodStrategies[method](userInfo)
-      expect(requestData.model).toEqual(method)
+      const requestData = methodStrategies[Methods.SMS](userInfo)
+      expect(requestData.model).toEqual(Methods.SMS)
       expect(requestData.damnParamA).toBeDefined()
       expect(requestData.userCode).toEqual(userInfo.userId)
       expect(requestData.cellphoneNumber).toEqual(userInfo.userPhone)
@@ -29,12 +28,11 @@ describe('Strategy Pattern: Third-Party Login Methods', () => {
 
     test('when the type of manufacturer is MF1 and method type is PIN', () => {
       const userInfo = getUserInfo()
-      const method = 'PIN'
-      const form = {
+      const form: Form = {
         pinValue: '9527',
       }
-      const requestData = methodStrategies[method](userInfo, form)
-      expect(requestData.model).toEqual(method)
+      const requestData = methodStrategies[Methods.PIN](userInfo, form)
+      expect(requestData.model).toEqual(Methods.PIN)
       expect(requestData.damnParamA).toBeDefined()
       expect(requestData.userCode).toEqual(userInfo.userId)
       expect(requestData.pinValue).toEqual(form.pinValue)
@@ -44,18 +42,16 @@ describe('Strategy Pattern: Third-Party Login Methods', () => {
   describe('when the type of manufacturer is MF2', () => {
     test('when the type of manufacturer is MF2 and method type is XXX_CODE', () => {
       const userInfo = getUserInfo()
-      const method = 'XXX_CODE'
-      const requestData = methodStrategies[method](userInfo)
-      expect(requestData.method).toEqual(method)
+      const requestData = methodStrategies[Methods.XXX_CODE](userInfo)
+      expect(requestData.method).toEqual(Methods.XXX_CODE)
       expect(requestData.damnParamB).toBeDefined()
       expect(requestData.account).toEqual(userInfo.userId)
     })
 
     test('when the type of manufacturer is MF2 and method type is XXX_SMS', () => {
       const userInfo = getUserInfo()
-      const method = 'XXX_SMS'
-      const requestData = methodStrategies[method](userInfo)
-      expect(requestData.method).toEqual(method)
+      const requestData = methodStrategies[Methods.XXX_SMS](userInfo)
+      expect(requestData.method).toEqual(Methods.XXX_SMS)
       expect(requestData.damnParamB).toBeDefined()
       expect(requestData.account).toEqual(userInfo.userId)
       expect(requestData.phone).toEqual(userInfo.userPhone)
@@ -63,12 +59,11 @@ describe('Strategy Pattern: Third-Party Login Methods', () => {
 
     test('when the type of manufacturer is MF2 and method type is XXX_PIN', () => {
       const userInfo = getUserInfo()
-      const method = 'XXX_PIN'
-      const form = {
+      const form: Form = {
         pinValue: '9527',
       }
-      const requestData = methodStrategies[method](userInfo, form)
-      expect(requestData.method).toEqual(method)
+      const requestData = methodStrategies[Methods.XXX_PIN](userInfo, form)
+      expect(requestData.method).toEqual(Methods.XXX_PIN)
       expect(requestData.damnParamB).toBeDefined()
       expect(requestData.account).toEqual(userInfo.userId)
       expect(requestData.pin).toEqual(form.pinValue)

bdd22a1ee64d5918a6ac66450acf1417a48d9a88
nauxscript <nauxscript@gmail.com>
2023-03-30 14:52:52 +0800

    test: add unit test

---
+++ examples/strategy-pattern/LoginMethods/test.spec.ts
@@ -0,0 +1,76 @@
+import { describe, expect, test } from 'vitest'
+
+const getUserInfo = () => ({
+  userId: 'root',
+  userPhone: '13838383838',
+})
+
+describe('Strategy Pattern: Third-Party Login Methods', () => {
+  describe('when the type of manufacturer is MF1', () => {
+    test('when the type of manufacturer is MF1 and method type is qrCode', () => {
+      const userInfo = getUserInfo()
+      const method = 'qrCode'
+      const requestData = getRequestData(method, userInfo)
+      expect(requestData.model).toEqual(method)
+      expect(requestData.damnParamA).toBeDefined()
+      expect(requestData.userCode).toEqual(userInfo.userId)
+    })
+
+    test('when the type of manufacturer is MF1 and method type is SMS', () => {
+      const userInfo = getUserInfo()
+      const method = 'SMS'
+      const requestData = getRequestData(method, userInfo)
+      expect(requestData.model).toEqual(method)
+      expect(requestData.damnParamA).toBeDefined()
+      expect(requestData.userCode).toEqual(userInfo.userId)
+      expect(requestData.cellphoneNumber).toEqual(userInfo.userPhone)
+    })
+
+    test('when the type of manufacturer is MF1 and method type is PIN', () => {
+      const userInfo = getUserInfo()
+      const method = 'PIN'
+      const form = {
+        pinValue: '9527',
+      }
+      const requestData = getRequestData(method, userInfo, form)
+      expect(requestData.model).toEqual(method)
+      expect(requestData.damnParamA).toBeDefined()
+      expect(requestData.userCode).toEqual(userInfo.userId)
+      expect(requestData.pinValue).toEqual(form.pinValue)
+    })
+  })
+
+  describe('when the type of manufacturer is MF2', () => {
+    test('when the type of manufacturer is MF2 and method type is XXX_CODE', () => {
+      const userInfo = getUserInfo()
+      const method = 'XXX_CODE'
+      const requestData = getRequestData(method, userInfo)
+      expect(requestData.method).toEqual(method)
+      expect(requestData.damnParamB).toBeDefined()
+      expect(requestData.account).toEqual(userInfo.userId)
+    })
+
+    test('when the type of manufacturer is MF2 and method type is XXX_SMS', () => {
+      const userInfo = getUserInfo()
+      const method = 'XXX_SMS'
+      const requestData = getRequestData(method, userInfo)
+      expect(requestData.method).toEqual(method)
+      expect(requestData.damnParamB).toBeDefined()
+      expect(requestData.account).toEqual(userInfo.userId)
+      expect(requestData.phone).toEqual(userInfo.userPhone)
+    })
+
+    test('when the type of manufacturer is MF2 and method type is XXX_PIN', () => {
+      const userInfo = getUserInfo()
+      const method = 'XXX_PIN'
+      const form = {
+        pinValue: '9527',
+      }
+      const requestData = getRequestData(method, userInfo, form)
+      expect(requestData.method).toEqual(method)
+      expect(requestData.damnParamB).toBeDefined()
+      expect(requestData.account).toEqual(userInfo.userId)
+      expect(requestData.pin).toEqual(form.pinValue)
+    })
+  })
+})
